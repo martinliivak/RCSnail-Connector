df55a0e1466347d0dc75b7dd2a6ee59cb02c6f68
martin.liivak <martin.liivak@gmail.com>
2019-11-29 14:25:03 +0200

    Clean up unneeded stuff from interceptor. Close sync connection

--- src/main.py
+++ src/main.py
@@ -26,42 +26,42 @@ def main(context: zmq.Context):
     rcs = RCSnail()
     rcs.sign_in_with_email_and_password(username, password)
 
-    publisher: zmq.Socket = sync_publisher(context)
+    data_queue: zmq.Socket = sync_publisher(context)
 
-    #loop = asyncio.get_event_loop()
-    #pygame_event_queue = asyncio.Queue()
-    #pygame.init()
-    #pygame.display.set_caption("RCSnail Connector")
-#
-    #config_manager = ConfigurationManager()
-    #config = config_manager.config
-    #screen = pygame.display.set_mode((config.window_width, config.window_height))
-#
-    #interceptor = Interceptor(config)
-#
-    #car = Car(config, update_override=interceptor.car_update_override)
-    #renderer = PygameRenderer(screen, car)
-    #interceptor.set_renderer(renderer)
-#
-    #executor = ThreadPoolExecutor(max_workers=32)
-    #pygame_task = loop.run_in_executor(executor, renderer.pygame_event_loop, loop, pygame_event_queue)
-    #render_task = asyncio.ensure_future(renderer.render(rcs))
-    #event_task = asyncio.ensure_future(renderer.register_pygame_events(pygame_event_queue))
-    #queue_task = asyncio.ensure_future(rcs.enqueue(loop, interceptor.intercept_frame, interceptor.intercept_telemetry))
-#
-    #try:
-    #    loop.run_forever()
-    #except KeyboardInterrupt:
-    #    print("Closing due to keyboard interrupt.")
-    #finally:
-    #    queue_task.cancel()
-    #    pygame_task.cancel()
-    #    render_task.cancel()
-    #    event_task.cancel()
-    #    pygame.quit()
-    #    asyncio.ensure_future(rcs.close_client_session())
-    #    #interceptor.close()
-    #    print("Shop closed.")
+    return
+    loop = asyncio.get_event_loop()
+    pygame_event_queue = asyncio.Queue()
+
+    pygame.init()
+    pygame.display.set_caption("RCSnail Connector")
+    config_manager = ConfigurationManager()
+    config = config_manager.config
+
+    screen = pygame.display.set_mode((config.window_width, config.window_height))
+    interceptor = Interceptor(config, data_queue)
+    car = Car(config, update_override=interceptor.car_update_override)
+    renderer = PygameRenderer(screen, car)
+    interceptor.set_renderer(renderer)
+
+    executor = ThreadPoolExecutor(max_workers=32)
+    pygame_task = loop.run_in_executor(executor, renderer.pygame_event_loop, loop, pygame_event_queue)
+    render_task = asyncio.ensure_future(renderer.render(rcs))
+    event_task = asyncio.ensure_future(renderer.register_pygame_events(pygame_event_queue))
+    queue_task = asyncio.ensure_future(rcs.enqueue(loop, interceptor.intercept_frame, interceptor.intercept_telemetry))
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print("Closing due to keyboard interrupt.")
+    finally:
+        queue_task.cancel()
+        pygame_task.cancel()
+        render_task.cancel()
+        event_task.cancel()
+        pygame.quit()
+        asyncio.ensure_future(rcs.close_client_session())
+        #interceptor.close()
+        print("Shop closed.")
 
 
 def sync_publisher(context: zmq.Context):
@@ -69,23 +69,21 @@ def sync_publisher(context: zmq.Context):
     publisher.sndhwm = 1100000
     publisher.bind('tcp://*:5561')
 
-    sync_service = context.socket(zmq.REP)
-    sync_service.bind('tcp://*:5562')
-
-    print("connection is bound")
+    synchronizer = context.socket(zmq.REP)
+    synchronizer.bind('tcp://*:5562')
 
     subscribers = 0
     while subscribers < 1:
         publisher.send(b'')
-        msg = sync_service.recv()
+        msg = synchronizer.recv()
         print(msg)
-        sync_service.send(b'')
+        synchronizer.send(b'')
         subscribers += 1
 
     for i in range(10):
         publisher.send(b'Beep')
 
-    publisher.send(b'END')
+    synchronizer.close()
     return publisher
 
 
--- src/pipeline/interceptor.py
+++ src/pipeline/interceptor.py
@@ -1,24 +1,23 @@
 import numpy as np
-from multiprocessing import Process, Queue, Event
-from queue import Empty
+from multiprocessing import Event
 
 from src.utilities.car_controls import CarControls, CarControlDiffs
-from utilities.message import Message
+from src.utilities.message import Message
+from zmq import Socket
 
 
 class Interceptor:
-    def __init__(self, configuration, recorder=None):
+    def __init__(self, configuration, data_queue: Socket):
         self.kill_event = Event()
         self.renderer = None
-        self.recorder = recorder
         self.resolution = (configuration.recording_width, configuration.recording_height)
+        self.publisher = data_queue
 
         self.frame = None
         self.telemetry = None
         self.expert_updates = CarControlDiffs(0, 0.0, 0.0, 0.0)
         self.car_controls = CarControls(0, 0.0, 0.0, 0.0)
 
-        self.recording_enabled = self.recorder is not None and configuration.recording_enabled
         self.runtime_training_enabled = configuration.runtime_training_enabled
         self.model_override_enabled = configuration.model_override_enabled
 
@@ -34,42 +33,21 @@ class Interceptor:
         if frame is not None:
             self.frame = self.__convert_frame(frame)
 
-            if self.recording_enabled:
-                self.__record_state()
-            elif self.runtime_training_enabled:
-                self.aggregation_count += self.__record_state_with_expert()
-
-    def intercept_telemetry(self, telemetry):
-        self.telemetry = telemetry
-
     def __convert_frame(self, frame):
         return np.array(frame.to_image().resize(self.resolution)).astype(np.float32)
 
-    def __record_state(self):
-        self.recorder.record(self.frame, self.telemetry)
-
-    def __record_state_with_expert(self):
-        return self.recorder.record_expert(self.frame, self.telemetry, self.expert_updates)
+    def intercept_telemetry(self, telemetry):
+        self.telemetry = telemetry
 
     async def car_update_override(self, car):
         try:
             self.expert_updates = CarControlDiffs(car.gear, car.d_steering, car.d_throttle, car.d_braking)
             self.car_controls = CarControls(car.gear, car.steering, car.throttle, car.braking)
 
-            if self.runtime_training_enabled and self.aggregation_count > 0 and ((self.aggregation_count // 2) % 200) == 0:
-                frames, telemetry, expert_actions = self.recorder.get_current_data()
-                print(len(frames))
-                print(len(telemetry))
-                print(len(expert_actions))
-
             if self.model_override_enabled and self.frame is not None and self.telemetry is not None:
                 self.__update_car_from_predictions(car)
         except Exception as ex:
             print("Override exception: {}".format(ex))
-
-    def __start_fitting_model(self, train_tuple, test_tuple):
-        if not self.kill_event.is_set():
-            self.model_queue.put(Message("training", (train_tuple, test_tuple)))
 
     def __update_car_from_predictions(self, car):
         try:
